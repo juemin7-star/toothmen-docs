3dfbc7c8b8a0349d474b274fa5ef466c2ab63394
ToothMen <zhaoshichuan88@126.com>
2026-06-05 22:00:08 +0800

    文档站更新 2026-06-05 22:00:08

--- sidebars.js
+++ sidebars.js
@@ -105,6 +105,7 @@ const sidebars = {
       type: 'category',
       label: '2026',
       items: [
+        'changelog/2026/20260605',
         'changelog/2026/20260604',
         'changelog/2026/20260603',
         'changelog/2026/20260526',
